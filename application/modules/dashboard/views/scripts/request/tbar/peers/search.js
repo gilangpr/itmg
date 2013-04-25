49ce7eaaff2b6245cc3f76f0344a35e8a56ce3cb
myindo <myindo@myindo06.langsat.myindo.net>
2013-04-25 14:56:46 +0700

    rezza

--- application/modules/dashboard/views/scripts/request/tbar/peers/search.js
+++ application/modules/dashboard/views/scripts/request/tbar/peers/search.js
@@ -1,4 +1,6 @@
 var c = Ext.getCmp('<?php echo $this->container ?>');
+
+var selected = c.getSelectionModel().getSelection();
 //var storeSP = loadStore('Peers');
 var storeSP = Ext.create('Ext.data.Store',{
     storeId: 'Peers2',
@@ -53,13 +55,72 @@ Ext.create('Ext.Window', {
 		listeners: {
 			click: function() {
 				var form = Ext.getCmp('search-peers-form')
-				console.log()
+				var _id = 'peers-search-result-' + Math.random();
 				if(form.getForm().isValid()) {
-					var _store = loadStore('Peers');
-					_store.load({
-						params: {
-							type: 'search',
-							name: Ext.getCmp('peers-search-peer-name').getValue()
+					var _storePeers = Ext.create("Ext.data.Store", {
+						model: "Peer",
+						storeId: "Peers",
+						proxy:{"type":"ajax","api":{"read":"\/peers\/request\/read","create":"\/peers\/request\/create","update":"\/peers\/request\/update","destroy":"\/peers\/request\/destroy"},"actionMethods":{"create":"POST","destroy":"POST","read":"POST","update":"POST"},"reader":{"idProperty":"DATE","type":"json","root":"data.items","totalProperty":"data.totalCount"},"writer":{"type":"json","root":"data","writeAllFields":true},
+							extraParams: {
+								type: 'search',
+								name: Ext.getCmp('peers-search-peer-name').getValue()
+							}},
+						sorter: {"property":"SHAREPRICES_ID","direction":"ASC"}});
+					showLoadingWindow();
+					_storePeers.load({
+						callback: function(d,i,e,f) {
+							c.up().add({
+								title: 'Shareprices Search Result',
+								closable: true,
+								id: _id,
+								autoScroll: true,
+								xtype: 'gridpanel',
+								layout: 'border',
+								store: _storePeers,
+								tbar : [{
+									xtype: 'button',
+		                            text: 'View Peer Detail',
+		                            iconCls: 'icon-detail',
+		                            listeners: {
+		                            	click: function() {
+		                            		if(selected.length > 0) {
+		                            			
+		                            		}
+		                            	}
+		                            }
+								}],
+								 "columns": [{
+	                                    "text": "Peer Name",
+	                                    "dataIndex": "PEER_NAME",
+	                                    "align": "left",
+	                                    "width": 250,
+	                                    "flex": 1,
+	                                    "dataType": "string",
+	                                    "visible": false,
+	                                    "editor": {
+	                                        "allowBlank": false
+	                                    }
+	                                }, {
+	                                    "text": "Created Date",
+	                                    "dataIndex": "CREATED_DATE",
+	                                    "align": "center",
+	                                    "width": 150,
+	                                    "flex": 0,
+	                                    "dataType": "string",
+	                                    "visible": false
+	                                }, {
+	                                    "text": "Modified Date",
+	                                    "dataIndex": "MODIFIED_DATE",
+	                                    "align": "center",
+	                                    "width": 150,
+	                                    "flex": 0,
+	                                    "dataType": "string",
+	                                    "visible": false
+	                                }
+	                            ]
+							});
+							c.up().setActiveTab(_id);
+							closeLoadingWindow();
 						}
 					});
 					this.up().up().close();
