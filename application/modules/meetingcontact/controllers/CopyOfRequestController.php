a697f18fd7d7c939d985c7b0ff11e6bf43b978e6
myindo <myindo@myindo06.langsat.myindo.net>
2013-04-27 02:07:37 +0700

    rezza

--- application/modules/meetingcontact/controllers/CopyOfRequestController.php
+++ application/modules/meetingcontact/controllers/CopyOfRequestController.php
@@ -131,49 +131,84 @@ class Meetingcontact_RequestController extends Zend_Controller_Action
 	}
 	public function readAction()
 	{
-		$name = $this->_model->getName($this->_posts['id']);
-		$modPart = new Application_Model_Participant();
-		$namePart = $modPart->getName($this->_posts['id']);
-		$_i = 0;
+		/*$ma_id = (isset($this->_posts['id'])) ? $this->_posts['id'] : 0;
+		$full = $this->_model->getListMeetingcontactLimit($this->_limit, $this->_start,$ma_id);
+		$data = array(
+				'data' => array(
+				'items' => $full,
+						'totalCount' => $this->_model->count()
+				)
+		);
+		MyIndo_Tools_Return::JSON($data, $this->_error_code, $this->_error_message, $this->_success);*/
+		$ma_id = (isset($this->_posts['id'])) ? $this->_posts['id'] : 0;
+		$full = $this->_model->getListMeetingcontactLimit($this->_limit, $this->_start,$ma_id);
 		$_temp = '';
-		foreach ($name as $k=>$d) {
-			//print_r($d);
-			if($_temp == '') {
-				$_temp = $d['CONTACT_ID'];
-				$this->_data['data']['items'][$_i]['NAME'] = '';
-			}
-			if($_temp != $d['CONTACT_ID']) {
-				$_i++;
-				$_temp = $d['CONTACT_ID'];
-				$this->_data['data']['items'][$_i]['NAME'] = '';
-			}
-			if(!isset($this->_data['data']['items'][$_i]['CONTACT_ID'])) {
-				$this->_data['data']['items'][$_i]['NAME'] = $d['NAME'];
-				$this->_data['data']['items'][$_i]['EMAIL'] = $d['EMAIL'];
-				$this->_data['data']['items'][$_i]['POSITION'] = $d['POSITION'];
-				$this->_data['data']['items'][$_i]['COMPANY_NAME'] = $d['COMPANY_NAME']; 					
-			}
-			
-			//print_r(count($_i));
-			//$_j=$_i+2;
-			$_j = $_i+1;
-			$_temp2 = '';
-			foreach ($namePart as $_k=>$_d) {
-				if($_temp2 == '') {
-					$_temp2 = $_d['PART_ID'];
-				}
-				if($_temp2 != $_d['PART_ID']) {
-					$_j++;
-					$_temp2 = $_d['PART_ID'];
-				}
-				if(!isset($this->_data['data']['items'][$_j]['PART_ID'])) {
-					$this->_data['data']['items'][$_j]['NAME'] = $_d['NAME'];
-					$this->_data['data']['items'][$_j]['EMAIL'] = $_d['EMAIL'];
-					$this->_data['data']['items'][$_j]['POSITION'] = $_d['POSITION'];
-				}
-			}
-		}
-		
+		//$_temp2 = '';
+		$_i = 0;
+		foreach ($full as $k=>$d) {
+            if($_temp == '') {
+                $_temp = $d['CONTACT_ID'];
+                $this->_data['data']['items'][$_i]['COMPANY_NAME'] = '';
+                $this->_data['data']['items'][$_i]['NAME'] = '';
+                $this->_data['data']['items'][$_i]['EMAIL'] = '';
+                $this->_data['data']['items'][$_i]['POSITION'] = '';
+            }
+            if($_temp != $d['CONTACT_ID']) {
+                $_i++;
+                $_temp = $d['CONTACT_ID'];
+                $this->_data['data']['items'][$_i]['COMPANY_NAME'] = '';
+                $this->_data['data']['items'][$_i]['NAME'] = '';
+                $this->_data['data']['items'][$_i]['EMAIL'] = '';
+                $this->_data['data']['items'][$_i]['POSITION'] = '';
+            }
+            if(!isset($this->_data['data']['items'][$_i]['CONTACT_ID'])) {
+                //$originalDate = $d['MEETING_DATE'];
+                //$newDate = date("d-m-Y", strtotime($originalDate));
+                $this->_data['data']['items'][$_i]['MEETING_ACTIVITIE_ID'] = $d['MEETING_ACTIVITIE_ID'];
+                $this->_data['data']['items'][$_i]['CONTACT_ID'] = $d['CONTACT_ID'];
+                $this->_data['data']['items'][$_i]['COMPANY_NAME'] = $d['COMPANY_NAME'];
+                $this->_data['data']['items'][$_i]['NAME'] = $d['NAME'];
+                $this->_data['data']['items'][$_i]['EMAIL'] = $d['EMAIL'];
+                $this->_data['data']['items'][$_i]['POSITION'] = $d['POSITION'];
+            }
+            
+        }
+       
+		//print_r($partName);
+		//$_temp = '';
+		$participant = new Application_Model_Participant();
+		$partName = $participant->getListParticipant($ma_id);
+		$_temp2 = '';
+		$_j = 0;
+		foreach ($partName as $_k=>$_d) {
+            if($_temp2 == '') {
+                $_temp2 = $_d['PART_ID'];
+                $this->_data['data']['items'][$_j]['COMPANY_NAME'] = '';
+                $this->_data['data']['items'][$_j]['NAME'] = '';
+                $this->_data['data']['items'][$_j]['EMAIL'] = '';
+                $this->_data['data']['items'][$_j]['POSITION'] = '';
+            }
+            if($_temp2 != $_d['PART_ID']) {
+                $_j++;
+                $_temp2 = $_d['PART_ID'];
+                $this->_data['data']['items'][$_j]['COMPANY_NAME'] = '';
+                $this->_data['data']['items'][$_j]['NAME'] = '';
+                $this->_data['data']['items'][$_j]['EMAIL'] = '';
+                $this->_data['data']['items'][$_j]['POSITION'] = '';
+            }
+            if(!isset($this->_data['data']['items'][$_j]['PART_ID'])) {
+                //$originalDate = $d['MEETING_DATE'];
+                //$newDate = date("d-m-Y", strtotime($originalDate));
+                $this->_data['data']['items'][$_j]['MEETING_ACTIVITIE_ID'] = $_d['MEETING_ACTIVITIE_ID'];
+                $this->_data['data']['items'][$_j]['PART_ID'] = $_d['PART_ID'];
+                //$this->_data['data']['items'][$_j]['COMPANY_NAME'] = '';
+                $this->_data['data']['items'][$_j]['NAME'] = $_d['NAME'];
+                $this->_data['data']['items'][$_j]['EMAIL'] = $_d['EMAIL_PARTICIPANT'];
+                $this->_data['data']['items'][$_j]['POSITION'] = $_d['POSITION_PARTICIPANT'];
+            }
+            
+        }
+       
 		$this->_data['data']['totalCount'] = $this->_model->count();
 		MyIndo_Tools_Return::JSON($this->_data, $this->_error_code, $this->_error_message, $this->_success);
 
