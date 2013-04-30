ea7da0e28e7965da08db30aaf491def7a515abf8
gilang <gilang.pratama@myindo.co.id>
2013-04-30 17:09:08 +0700

    gilang

--- application/modules/investorstatus/controllers/RequestController.php
+++ application/modules/investorstatus/controllers/RequestController.php
@@ -1,6 +1,6 @@
 <?php 
 
-class Investorstatus_RequestController extends Zend_Controller_Action
+class Investorstatus_RequestController extends MyIndo_Controller_Action
 {
 	protected $_model;
 	protected $_limit;
@@ -33,32 +33,62 @@ class Investorstatus_RequestController extends Zend_Controller_Action
 				'data' => array(
 						'items' => array(),
 						'totalCount' => 0
-				)
-		);
+						)
+				);
 	}
 	
 	public function  readAction() 
 	{
-		$data = array(
-				'data' => array(
-						'items' => $this->_model->getListLimit($this->_limit, $this->_start, 'INVESTOR_STATUS ASC'),
-						'totalCount' => $this->_model->count()
-				)
-		);
+		if($this->isPost() && $this->isAjax()) {
+			if(isset($this->_posts['sort']) || isset($this->_posts['query'])) {
+				try {
+					if(isset($this->_posts['sort'])) {
+						// Decode sort JSON :
+						$sort = Zend_Json::decode($this->_posts['sort']);
+					}
+					// Query data
+					$q = $this->_model->select();
+						
+					if(isset($this->_posts['sort'])) {
+						$q->order($sort[0]['property'] . ' ' . $sort[0]['direction']);
+					}
+						
+					if(isset($this->_posts['query'])) {
+						if(!empty($this->_posts['query']) && $this->_posts['query'] != '') {
+							$q->where('INVESTOR_STATUS LIKE ?', '%' . $this->_posts['query'] . '%');
+						}
+					}
+						
+					// Count all data
+					$rTotal = $q->query()->fetchAll();
+					$totalCount = count($rTotal);
+						
+					// Fetch sorted & limit data
+					$q->limit($this->_limit, $this->_start);
+					$result = $q->query()->fetchAll();
+						
+					$this->_data['data']['items'] = $result;
+					$this->_data['data']['totalCount'] = $totalCount;
+				} catch (Exception $e) {
+					$this->_error_code = $e->getCode();
+					$this->_error_message = $e->getMessage();
+					$this->_success = false;
+				}
+			} else {
+				$this->_data['data']['items'] = $this->_model->getListLimit($this->_limit, $this->_start);
+				$this->_data['data']['totalCount'] = $this->_model->count();
+			}
+		} else {
+			$this->error(901);
+		}
+		$this->json();
+	}	
 		
-		MyIndo_Tools_Return::JSON($data, $this->_error_code, $this->_error_message, $this->_success);
-		}	
-		
-		public function createAction() {
-		
-			$data = array(
-					'data' => array()
-			);
-
+	public function createAction()
+	{
+		if($this->isPost() && $this->isAjax()) {
 			if($this->_model->isExistByKey('INVESTOR_STATUS', $this->_posts['INVESTOR_STATUS'])) {
-					
-				$this->_success = false;
-				$this->_error_message = 'Investor Status already exist.';
+				$this->error(201);
 			} else {
 				try {
 					// Do insert query :
@@ -73,98 +103,87 @@ class Investorstatus_RequestController extends Zend_Controller_Action
 					$this->_success = false;
 				}
 			}
-			
-			MyIndo_Tools_Return::JSON($data, $this->_error_code, $this->_error_message, $this->_success);
+		} else {
+			$this->error(901);
 		}
+		$this->json();
+	}
 		
-		public function updateAction()
-		{
-		
-			$data = array(
-					'data' => array()
-			);
-		
+	public function updateAction()
+	{
+		if($this->isPost() && $this->isAjax()) {
 			$data = $this->getRequest()->getRawBody();//mengambil data json
 			$data = Zend_Json::decode($data);//merubah data json menjadi array
 			$id = $data['data']['INVESTOR_STATUS_ID'];
 		
 			try {
-				
-				
-			if(!$this->_model->isExistByKey('INVESTOR_STATUS', $data['data']['INVESTOR_STATUS'])){
-				$this->_model->update(array(
+				if(!$this->_model->isExistByKey('INVESTOR_STATUS', $data['data']['INVESTOR_STATUS'])){
+					$this->_model->update(array(
 						'INVESTOR_STATUS' => $data['data']['INVESTOR_STATUS'],
-				),$this->_model->getAdapter()->quoteInto('INVESTOR_STATUS_ID = ?', $id));
+					),$this->_model->getAdapter()->quoteInto('INVESTOR_STATUS_ID = ?', $id));
 				} else {
-					$this->_error_message = 'Data Being Used';
-					$this->_success = false;
+					$this->error(202);
 				}
-					
 			}catch(Exception $e) {
 				$this->_error_code = $e->getCode();
 				$this->_error_message = $e->getMessage();
 				$this->_success = false;
 			}
-		
-			MyIndo_Tools_Return::JSON($data, $this->_error_code, $this->_error_message, $this->_success);
+		} else {
+			$this->error(901);
 		}
+		$this->json();
+	}
 		
-		public function destroyAction()
-		{
-			$data = array(
-					'data' => array()
-			);
+	public function destroyAction()
+	{
+		if($this->isPost() && $this->isAjax()) {
 			try {
 				// Delete
 				$share = new Application_Model_Shareholdings();
 				$val = $this->_model->getValueByKey('INVESTOR_STATUS_ID', $this->_posts['INVESTOR_STATUS_ID'], 'INVESTOR_STATUS');
 				$query = $share->select()
 				->where('INVESTOR_STATUS_ID = ?', $this->_posts['INVESTOR_STATUS_ID']);
-
-                if ($query->query()->rowCount() > 0) {
-                		
-                		$this->_error_message = 'Delete failed, data is being used.';
-			            $this->_success = false;
-                		
-                } else {
-                	
-                	$this->_model->delete(
-                			$this->_model->getAdapter()->quoteInto(
-                					'INVESTOR_STATUS_ID = ?', $this->_posts['INVESTOR_STATUS_ID']
-                			));
-                	
-                	$this->_error_message = 'Data successfully deleted.';
-                	$this->_success = false;
-               
-				}
-			}
-			catch(Exception $e) {
 				
+				$total = $query->query()->fetchAll();
+				if (count($total) > 0) {
+		        	$this->error(202);
+		        } else {
+		        	$this->_model->delete(
+		            $this->_model->getAdapter()->quoteInto(
+		            	'INVESTOR_STATUS_ID = ?', $this->_posts['INVESTOR_STATUS_ID']
+		            ));
+		        	$this->_error_message = 'Data successfully deleted.';
+		        	$this->_success = false;
+				}
+			} catch(Exception $e) {
 				$this->_error_code = $e->getCode();
 				$this->_error_message = $e->getMessage();
 				$this->_success = false;
 			}
-			MyIndo_Tools_Return::JSON($data, $this->_error_code, $this->_error_message, $this->_success);
+		} else {
+			$this->error(901);
 		}
+		$this->json();
+	}
 		
-		public function  autocomAction() 
-		{
-			if ($this->_posts['query'] == '') {
- 			
- 			$data = array(
- 					'data' => array(
- 							'items' => $this->_model->getListLimit($this->_limit, $this->_start, 'INVESTOR_STATUS ASC'),
- 							'totalCount' => $this->_model->count()
- 							)
- 					);
+	public function  autocomAction() 
+	{
+		if ($this->_posts['query'] == '') {
+	 		$this->_data = array(
+	 			'data' => array(
+	 				'items' => $this->_model->getListLimit($this->_limit, $this->_start, 'INVESTOR_STATUS ASC'),
+	 				'totalCount' => $this->_model->count()
+	 			)
+	 		);
  		} else {
-				$data = array(
-						'data' => array(
-								'items' => $this->_model->getAllLike($this->_posts['query'], $this->_limit, $this->_start),
-								'totalCount' => $this->_model->count()
-						)
-				);
-			}
-			MyIndo_Tools_Return::JSON($data, $this->_error_code, $this->_error_message, $this->_success);
+			$this->_data = array(
+				'data' => array(
+					'items' => $this->_model->getAllLike($this->_posts['query'], $this->_limit, $this->_start),
+					'totalCount' => $this->_model->count()
+				)
+			);
 		}
+		$this->json();
+	}	
 }
